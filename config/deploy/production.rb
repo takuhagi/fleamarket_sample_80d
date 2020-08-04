eb3a62fa8bcb1bd68305f06ee2c93c5888ae8dbb
takuhagi <h.takumi52@gmail.com>
2020-08-05 00:18:54 +0900

    デプロイ作業

---
+++ config/deploy/production.rb
@@ -0,0 +1,3 @@
+# 元々の記述は全て削除して大丈夫です
+server '54.249.185.167', user: 'ec2-user', roles: %w{app db web}
+# 自身のElasticIPの部分は書き換えてください
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# before
+get 'items/index'
+
+# after
+root 'items#index'
+
 end
