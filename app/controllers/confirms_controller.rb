c53d0311d569c077fa2092af0bac3df3c98578bb
masa-ki11 <67939220+masa-ki11@users.noreply.github.com>
2020-08-13 16:09:09 +0900

    追加実装

--- app/controllers/confirms_controller.rb
+++ app/controllers/confirms_controller.rb
@@ -1,14 +1,15 @@
 class ConfirmsController < ApplicationController
 
 def index
-  card = CreditCard.find(11) #仮
+  @user = Profile.where(user_id: current_user.id).first
+  card = CreditCard.where(user_id: current_user.id).first
   Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
   customer = Payjp::Customer.retrieve(card.customer_id)
   @credit_card = customer.cards.retrieve(card.credit_card_id)
 end
 
 def pay
-  card = CreditCard.find(11)  #仮
+  card = CreditCard.where(user_id: current_user.id).first
   Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
   Payjp::Charge.create(
     amount: 9000000, #仮
