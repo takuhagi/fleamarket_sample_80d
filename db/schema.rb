fdd5046ab45d587fd766fc875705c8ef0ea05ead
shin0414 <ooparts.0414@gmail.com>
2020-08-18 21:26:50 +0900

    商品詳細update

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_08_12_121247) do
+ActiveRecord::Schema.define(version: 2020_08_14_030527) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
